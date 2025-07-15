dbb4a5b9062d2f0f7bfd698af36323cc25dcdf4a
janphilippgutt <janphilippgutt@posteo.de>
2025-07-15 08:29:15 +0200

    Fix formatting issues for Terraform files

--- examples/vpc-basic/main.tf
+++ examples/vpc-basic/main.tf
@@ -13,8 +13,8 @@ provider "aws" {
 module "vpc" {
   source = "../../vpc"
 
-  name = "devops-vpc"
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  name                 = "devops-vpc"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -10,12 +10,12 @@ variable "name" {
 
 variable "enable_dns_support" {
   description = "Enable DNS support in the VPC"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "enable_dns_hostnames" {
   description = "Enable DNS hostnames in the VPC"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
