8e0ce3631099ac7779ce758617f2b755a3e7a9fb
janphilippgutt <janphilippgutt@posteo.de>
2025-08-10 09:11:12 +0200

    Add bucket_id output to s3-static-site module

--- modules/s3-static-site/outputs.tf
+++ modules/s3-static-site/outputs.tf
@@ -3,6 +3,11 @@ output "bucket_name" {
   value = aws_s3_bucket.static_site.bucket
 }
 
+output "bucket_id" {
+  description = "Id of the created S3 bucket"
+  value = aws_s3_bucket.static_site.id
+}
+
 output "website_endpoint" {
   description = "Website endpoint of the static site"
   value = aws_s3_bucket_website_configuration.static_site.website_endpoint
