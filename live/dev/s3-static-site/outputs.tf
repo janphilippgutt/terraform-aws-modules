ae0637fabe9dd358027c1ed0629ce625cbe9c6ca
janphilippgutt <janphilippgutt@posteo.de>
2025-09-14 16:00:29 +0200

    Add s3-static-site outputs to be used in cloudfront stack

--- live/dev/s3-static-site/outputs.tf
+++ live/dev/s3-static-site/outputs.tf
@@ -5,3 +5,7 @@ output "bucket_arn" {
 output "bucket_name" {
   value = module.s3_static_site.bucket_name
 }
+
+output "bucket_domain_name" {
+  value = module.s3_static_site.bucket_regional_domain_name
+}
